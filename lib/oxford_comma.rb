0d9330c65ee17b2ca4d794649382d8a4859fe795
Evan Schechter <schechter.evan@gmail.com>
2019-05-10 09:59:49 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    retString = ""
+    for ele in array[0,array.size - 1]
+      retString += "#{ele}, "
+    end
+    retString += "and #{array.last}"
+    return retString
+  end
 end
