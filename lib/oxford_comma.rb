f05098d4d886f24c12065274a953b0c15b1fd151
Alexander Riccio <test35965@gmail.com>
2019-05-20 16:03:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.length() == 1
+    return "#{array[0]}"
+  end
+  fmt_str = ""
+  
+  array.each_with_index do |elem, index|
+    if index == array.length - 1
+      fmt_str += "and "
+    end
+    fmt_str += elem
+    if array.length > 2
+      fmt_str += ", "
+    else
+      fmt_str += " "
+    end
+  end
+  fmt_str = fmt_str.chomp(", ").chomp(" ")
 end
